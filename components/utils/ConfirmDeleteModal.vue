05ab8358be20cc38c8404db5e83b16f486aed914
Suraj Jha <surajbeston@gmail.com>
2022-12-27 21:17:19 +0545

    async dropdown on manufacturer

--- components/utils/ConfirmDeleteModal.vue
+++ components/utils/ConfirmDeleteModal.vue
@@ -32,7 +32,6 @@
                                 @click="deleteSelected()">Yes</button>
                         </div>
                         <div v-else class="flex justify-center">
-
                             <svg aria-hidden="true"
                                 class="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
                                 viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -70,7 +69,6 @@ async function deleteSelected() {
     deleting.value = false
     emit('deleted', true)
 }
-
 
 function cancel() {
     emit("cancelled", true)
--- pages/supplier/[id].vue
+++ pages/supplier/[id].vue
@@ -1,1 +1,17 @@
-<template>Hello i am id</template>
+<template>
+    <div>
+        <AsyncDropDown objectsUrl="/meds/manufacturer/with_initial/" nameAttribute="name" valueAttribute="id"
+            @selectedObject="selectManufacturer">
+        </AsyncDropDown>
+    </div>
+</template>
+
+<script setup>
+import AsyncDropDown from '~/components/utils/AsyncDropdown.vue';
+
+function selectManufacturer(manufacturer) {
+    console.log(manufacturer)
+
+}
+
+</script>
